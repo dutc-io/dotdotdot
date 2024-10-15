73c4468cb3ac43968f0d01b2747577d775592e7e
Cameron Riddell <cameron@dutc.io>
2024-10-15 10:16:31 -0400

    update code

--- dotdotdot.py
+++ dotdotdot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from cv2 import HoughCircles, HOUGH_GRADIENT, imread, IMREAD_GRAYSCALE
+import cv2
+from cv2 import HoughCircles, HOUGH_GRADIENT, imread, IMREAD_GRAYSCALE, IMREAD_UNCHANGED
+import numpy as np
 from pathlib import Path
 import click
 
@@ -37,6 +39,41 @@ def find(path):
         print("There is one dot.")
     else:
         print(f"There are {num} dots.")
+
+@cli.command()
+@click.argument('path', type=Path)
+def classify(path):
+    from ast import literal_eval
+
+    img = imread(path, cv2.IMREAD_COLOR)
+    dot_colors = {}
+    f = open('classify.json')
+    for line in f.read().strip().strip('{}').strip().splitlines():
+        if not line: continue
+        name, values = line.rstrip(',').split(':')
+        name, values = literal_eval(name.strip()), literal_eval(values.strip())
+        dot_colors[name] = [tuple(x) for x in values]
+
+    circles_by_color = {}
+    blur = cv2.medianBlur(img, 7)
+    for color, (lower, upper) in dot_colors.items():
+        mask = cv2.inRange(blur, lower, upper)
+        circles = HoughCircles(
+            image=mask,
+            method=HOUGH_GRADIENT,
+            dp=1,
+            minDist=80,
+            param1=20,
+            param2=8,
+        )
+        if circles is None:
+            circles_by_color[color] = None
+        else:
+            circles_by_color[color] = np.round(circles).astype("int")
+
+    for color, dots in circles_by_color.items():
+        num = dots.shape[1] if dots is not None else 0
+        print('{color.title()} Dots: {num}')
 
 
 if __name__ == "__main__":
